43f6fdbd89dff9094e22f9e6b0e197f22afb7300
Yogesh <yogeshbietjhs@gmail.com>
2022-06-21 23:52:50 +0530

    employee wage 100 hours

--- EmpWage.cs
+++ EmpWage.cs
@@ -5,17 +5,22 @@
 
         public class EmpWage
         {
-        const int EmployeeFullTime = 1;
-        const int EmployeePartTime = 2;
-        const int MaxWorkedDay = 20;
-        const int EmpWagePerHour = 20;
+            const int EmployeeFullTime = 1;
+            const int EmployeePartTime = 2;
+            const int MaxWorkedDay = 20;
+            const int EmpWagePerHour = 20;
+            const int MaxWorkedHours = 100;
 
-        public void CheckEmp()
+            public void CheckEmp()
             {
                 int totalEmpWage = 0;
                 int EmpDailyHour;
+                int days = 0 , Total_Hours = 0;
+               
+
                 Random random = new Random();
-                for (int i = 0; i < MaxWorkedDay; i++)
+
+                for ( days = 0; days < MaxWorkedDay || MaxWorkedDay <= Total_Hours ; days++)
                 {
                     int Check = random.Next(3);
 
@@ -24,7 +29,8 @@
                         Console.WriteLine("Employee is present");
                         EmpDailyHour = 8;
                     }
-                    else if(Check == EmployeePartTime){
+                    else if (Check == EmployeePartTime)
+                    {
                         Console.WriteLine("Employee is part time");
                         EmpDailyHour = 4;
                     }
@@ -33,13 +39,16 @@
                         Console.WriteLine("Employee is Absent");
                         EmpDailyHour=0;
                     }
-                int DailyWage = EmpWagePerHour * EmpDailyHour;
-                Console.WriteLine(EmpDailyHour);
-                totalEmpWage += DailyWage;
+
+                    //Total_Hours += EmpDailyHour;
+                    int DailyWage = EmpWagePerHour * EmpDailyHour;
+                    Console.WriteLine("Day:" + days + "hhh " + DailyWage);
+                    totalEmpWage += DailyWage;
 
                 }
 
                 Console.WriteLine("TOTAL WAGE IS :" + totalEmpWage);
+                Console.WriteLine("Days Completed:" + days + "Total Hours Completed: " + Total_Hours );
 
 
 
