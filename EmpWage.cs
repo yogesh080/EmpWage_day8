24562cdebdc7a368a764e352d5b5c63ceb1b0b35
Yogesh <yogeshbietjhs@gmail.com>
2022-06-21 21:55:19 +0530

    inicial commit

---
+++ EmpWage.cs
@@ -0,0 +1,25 @@
+﻿namespace EmpWage
+{
+    internal partial class Program
+    {
+        public class EmpWage
+        {
+            public void CheckEmp()
+            {
+                int isPresent=1;
+                Random rand = new Random();
+                int Check = rand.Next(0,2);
+
+                if (Check == isPresent)
+                {
+                    Console.WriteLine("Employee is Present");
+                }
+                else
+                {
+                    Console.WriteLine("Absent");
+                }
+
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EmpWage
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWage empWage = new EmpWage();
+            empWage.CheckEmp();
+        }
+    }
+}
